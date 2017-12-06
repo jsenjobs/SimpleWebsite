31860d2c0ae25f9c45913bfbc0d184c8790ba39d
jsenjobs <jsen1922279340@163.com>
2017-12-06 21:24:27 +0800

    ok

---
+++ server.js
@@ -0,0 +1,16 @@
+
+var express = require('express')
+
+var PORT = 8888
+
+var app = express()
+
+app.get('/', (req, res) => {
+    res.status(200).send('Hello jsen\n')
+})
+
+app.listen(PORT, "localhost", err => {
+    console.error(err)
+})
+
+console.log('Server running on http://localhost:' + PORT)
